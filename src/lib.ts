c5ff2157bd5eb490390e878d753913355f578b8d
Marisa Robayo Moreno <marisarobayo@gmail.com>
2023-03-01 09:27:31 +0100

    Implement date conversion. (#2)

--- src/lib.ts
+++ src/lib.ts
@@ -1,2 +1,21 @@
 
-export {}
+import { Date as gDate } from '@buf/googleapis_googleapis.bufbuild_es/google/type/date_pb.js'
+
+export function serializeDate(date?: Date): gDate | undefined {
+  if (!date) {
+    return
+  }
+  return new gDate({
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+  })
+}
+
+export function parseDate(date?: gDate): Date | undefined {
+  if (!date) {
+    return
+  }
+  return new Date(date.year, date.month - 1, date.day)
+}
+
